593f0746fd9ddf84b25af93d9d727685462ed67f
Gkm3 <kirillakshentsev4@gmail.com>
2024-06-06 23:21:14 +0500

    auth and register

---
+++ db_conn.php
@@ -0,0 +1,11 @@
+<?php
+
+$host = 'localhost';
+$dbname = 'first_crud';
+$username = 'root';
+$password = '8U!zpuAxRarF';
+
+$pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
+
+?>
